f8ff3c81b20f835eae13e1fcdad55f1dab11169a
Nutdanai Srinithichsadaporn <koonmos1@gmail.com>
2023-04-24 08:46:05 +0000

    API for verify

--- index.py
+++ index.py
@@ -1,11 +1,20 @@
 # import streamlit as st
+# import os
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 from PIL import Image
 from deepface import DeepFace
 
-models = "Facenet"
+models = "DeepFace"
 
 def verify() :
-    result = DeepFace.verify(img1_path = "img1.jpg", img2_path = "img1.jpg", model_name = models)
-    print(result)
+    for i in range(0, 5) :
+        if(i == 0) :
+            print("1 : ", i, "\n")
+            result = DeepFace.verify(img1_path = "img1.jpg", img2_path = "img1.jpg", model_name = models)
+            print(result)
+        else :
+            print("2 : ", i, "\n")
+            result = DeepFace.verify(img1_path = "img2.jpg", img2_path = "img1.jpg", model_name = models)
+            print(result)
 
-Verify = verify()
+verify()
--- main.py
+++ main.py
@@ -1,15 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, Form
 from deepface import DeepFace
+import io
+import os
+from PIL import Image
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/verify/{img1,img2}")
-async def verify(img1, img2):
-    result = DeepFace.verify(img1_path = img1, img2_path = img2)
-    if(result.get("verified") == True) :
+@app.post("/verify")
+async def verify(image: UploadFile = File(...)):
+    img1_path = f"/tksgolry/Deepface/assets/{image.filename}"
+    img2_path = f'/tksgolry/Deepface/assets/Verify_{image.filename}'
+    image_bytes = await image.read()
+    image_pil = Image.open(io.BytesIO(image_bytes))
+    image_pil = image_pil.rotate(270, expand=True)
+    image_pil.save(img2_path)
+    try:
+        result = DeepFace.verify(img1_path=img1_path, img2_path=img2_path, model_name="Facenet512")
+    except ValueError as e:
+        if "Face could not be detected" in str(e):
+            os.remove(img2_path)
+            return {"message": "Error: " + str(e)}
+            # return {"message": "Error"}
+        else:
+            raise e
+    if result.get("verified") == True:
+        os.remove(img2_path)
         return {"message": "Face match!!"}
-    else :
+    else:
         return {"message": "Face not match!!"}
